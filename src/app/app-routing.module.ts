35c83c40c56bffa20e3a97510fcc889e1740629d
Lukinator3000 <Lukinator30000@gmail.com>
2023-07-10 20:56:18 -0400

    edited a few features and some new conditional logic with class binding and ngIf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomepageComponent,
+    title: "Homepage",
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
