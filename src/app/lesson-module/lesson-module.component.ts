b63066bcbc45e39933b14d9db1e20081b20c7242
Lukinator3000 <Lukinator30000@gmail.com>
2023-07-11 15:56:45 -0400

    added some new animations in the lesson-module components

--- src/app/lesson-module/lesson-module.component.ts
+++ src/app/lesson-module/lesson-module.component.ts
@@ -9,14 +9,14 @@ import { LessonModule } from '../lessonmodule';
         <h2>{{lessonModule.title}}</h2>
       </div>
       <ul>
-        <li>Study</li>
-        <li *ngIf="lessonModule.hasPracticeTests; else elseBlock">Practice</li>
+        <li class="arrow"><span>Study</span></li>
+        <li class="arrow" *ngIf="lessonModule.hasPracticeTests; else elseBlock">Practice</li>
         <ng-template #elseBlock>
-          <br><br>
+          <li class="no-arrow"></li>
         </ng-template>
       </ul>
       <div class="unit-foot">
-        <p class="resource">> Extra Resources</p>
+        <p class="resource">> <span class="arrow2">Extra Resources</span></p>
         <p class="check" [class.hide]="!lessonModule.completed">✓</p>
       </div>
     </div>
