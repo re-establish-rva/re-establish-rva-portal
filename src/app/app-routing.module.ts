8e10ca1ad94f228898336e09da0f24ca4e98f32b
Lukinator3000 <Lukinator30000@gmail.com>
2023-07-12 14:31:01 -0400

    added Jenny's code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { StudyComponent } from './study/study.component';
 
 const routes: Routes = [
   {
     path: "",
     component: HomepageComponent,
     title: "Homepage",
+  },
+  {
+    path: "study/:id",
+    component: StudyComponent,
+    title: "Study"
   }
 ];
 
--- src/app/study/study.component.ts
+++ src/app/study/study.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LessonService } from '../lesson.service';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { filter } from "rxjs/operators";
 
 @Component({
   selector: 'app-study',
-  templateUrl: './study.component.html',
+  template: `
+    <div class = "center">
+      <a [routerLink] = "['/']"><button class = "next" (click) = "1">Next</button></a>
+    </div>
+  `,
   styleUrls: ['./study.component.css']
 })
-export class StudyComponent {
 
+export class StudyComponent {
+  /* completeLesson(id: number): void {
+
+    this.lessonService.updateCompletion(id);
+    console.log("yay4: " + id);
+}
+  constructor(private router: Router, private lessonService:LessonService){
+
+  }
+ getId():number{
+    console.log(this.router.url);
+    this.router.events.pipe(filter((event): event is NavigationStart => event instanceof NavigationStart))
+          .subscribe(event =>
+          {
+            if (event.url.indexOf("/study/") >= 0) {
+              return(Number(event.url.substring(7)));
+            } 
+              //console.log("yay: " + event.url);
+              //console.log("yay2: " + this.currentId);
+              //console.log("yay3: " + event.url.substring(7));
+            });
+            return 0;
+  } */
 }
