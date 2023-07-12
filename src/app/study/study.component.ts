59df9dbf0271e18059c4c0fdf0929f136cbf16b3
Lukinator3000 <Lukinator30000@gmail.com>
2023-07-12 15:06:57 -0400

    completion feature started working

--- src/app/study/study.component.ts
+++ src/app/study/study.component.ts
@@ -1,39 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { LessonService } from '../lesson.service';
-import { NavigationEnd, NavigationStart, Router } from '@angular/router';
-import { filter } from "rxjs/operators";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-study',
   template: `
     <div class = "center">
-      <a [routerLink] = "['/']"><button class = "next" (click) = "1">Next</button></a>
+      <a [routerLink] = "['/']"><button class = "next" (click) = "completeLesson()">Next</button></a>
     </div>
   `,
   styleUrls: ['./study.component.css']
 })
 
 export class StudyComponent {
-  /* completeLesson(id: number): void {
-
-    this.lessonService.updateCompletion(id);
-    console.log("yay4: " + id);
-}
-  constructor(private router: Router, private lessonService:LessonService){
-
+  constructor(private route: ActivatedRoute, private lessonService: LessonService){
+    
   }
- getId():number{
-    console.log(this.router.url);
-    this.router.events.pipe(filter((event): event is NavigationStart => event instanceof NavigationStart))
-          .subscribe(event =>
-          {
-            if (event.url.indexOf("/study/") >= 0) {
-              return(Number(event.url.substring(7)));
-            } 
-              //console.log("yay: " + event.url);
-              //console.log("yay2: " + this.currentId);
-              //console.log("yay3: " + event.url.substring(7));
-            });
-            return 0;
-  } */
+
+  completeLesson(): void {
+    this.lessonService.updateCompletion(parseInt(this.route.snapshot.params['id'], 10));
+  }
 }
