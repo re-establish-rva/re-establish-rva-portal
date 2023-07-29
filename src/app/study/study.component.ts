d3e593de3b43d1b7313446c186116f82585eb07f
Lukinator3000 <Lukinator30000@gmail.com>
2023-07-29 18:52:37 -0400

    New redesign structure (added sidenav instead of grid elements)

--- src/app/study/study.component.ts
+++ src/app/study/study.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LessonService } from '../lesson.service';
 import { ActivatedRoute } from '@angular/router';
+import { LessonModule } from '../lessonmodule';
 
 @Component({
   selector: 'app-study',
@@ -10,11 +11,25 @@ import { ActivatedRoute } from '@angular/router';
 
 export class StudyComponent {
 
-  constructor(private route: ActivatedRoute, private lessonService: LessonService){}
+  moduleID: number;
+  lessonModuleList: LessonModule[] = [];
+
+  constructor(private route: ActivatedRoute, public lessonService: LessonService){
+    this.moduleID = parseInt(this.route.snapshot.params['id'], 10);
+  }
+
+  // Initializes lessonModuleList to the current array of LessonModules in LessonService
+  ngOnInit() {
+    this.lessonModuleList = this.lessonService.getAllLessonModules();
+  }
 
   // Calls LessonService to update the 'completed' attribute of the current LessonModule 
   completeLesson(): void {
-    this.lessonService.updateCompletion(parseInt(this.route.snapshot.params['id'], 10));
+    this.lessonService.updateCompletion(this.moduleID);
+  }
+
+  setActiveModuleID(id: number) {
+    this.moduleID = id;
   }
 
 }
